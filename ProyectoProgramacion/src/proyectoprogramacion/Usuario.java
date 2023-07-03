49b90366803c04f9fc7003209c742b2c91b9193e
thamaramora <134455787+thamaramora@users.noreply.github.com>
2023-07-02 23:56:14 -0600

    Atributos Usuario

--- ProyectoProgramacion/src/proyectoprogramacion/Usuario.java
+++ ProyectoProgramacion/src/proyectoprogramacion/Usuario.java
@@ -9,5 +9,22 @@ package proyectoprogramacion;
  * @author thama
  */
 public class Usuario {
+    private String identificacion;
+    private String nombre;
+    private String usuario;
+    private String correo;
+    private String contrasenia;
+
+    public Usuario(String identificacion, String nombre, String usuario, String correo, String contrasenia) {
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+    }
     
+    
+    
+
+
 }
