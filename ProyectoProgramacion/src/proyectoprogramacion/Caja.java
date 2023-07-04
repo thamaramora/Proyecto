c4eea81a5bc47902d2091c91610280358e5ccba4
Daniel <danhaddir@gmail.com>
2023-07-04 09:48:57 -0600

    Update Caja.java

--- ProyectoProgramacion/src/proyectoprogramacion/Caja.java
+++ ProyectoProgramacion/src/proyectoprogramacion/Caja.java
@@ -15,6 +15,17 @@ public class Caja {
         nombre = JOptionPane.showInputDialog("Ingrese su nombre:");
         edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad del usuario: "));
         cedula = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de cedula del usuario:"));
+        correo = JOptionPane.showInputDialog("Ingrese su correo electrónico:");
+        tramite = Integer.parseInt(JOptionPane.showInputDialog("""
+                                                               Ingrese el tramite que desea realizar: 
+                                                               1. Depositos
+                                                               2. Retiros
+                                                               3. Cambio de divisas"""));   
+    }
+    // Metodo
+    public void mostrarInformacion() {
+        String mensaje = """
+                         Informacion del usuario:
                          Nombre: """ + nombre + "\n" +
                         "Cédula: " + cedula + "\n" +
                         "Correo: " + correo + "\n" +
